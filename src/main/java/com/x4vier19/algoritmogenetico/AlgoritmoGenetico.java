55d1e9be13b255a084d4876b0135c1e88e11f39b
X4VIER19 <xavfer19@gmail.com>
2026-05-15 23:25:49 -0600

    Primer commit

---
+++ src/main/java/com/x4vier19/algoritmogenetico/AlgoritmoGenetico.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.x4vier19.algoritmogenetico;
+
+/**
+ *
+ * @author XAV
+ */
+public class AlgoritmoGenetico {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
