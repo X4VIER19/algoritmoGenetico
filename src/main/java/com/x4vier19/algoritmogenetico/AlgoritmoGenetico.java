07af4900a6b3a8894f36db9c81af1670d7c7bbf6
X4VIER19 <xavfer19@gmail.com>
2026-05-15 23:54:21 -0600

    feat: estructura base y generación de población inicial

--- src/main/java/com/x4vier19/algoritmogenetico/AlgoritmoGenetico.java
+++ src/main/java/com/x4vier19/algoritmogenetico/AlgoritmoGenetico.java
@@ -9,8 +9,37 @@ package com.x4vier19.algoritmogenetico;
  * @author XAV
  */
 public class AlgoritmoGenetico {
+    
+    static int numIndividuos = 100;
+    static int numGenes = 10;
+    static double probMutacion = 0.05;
+    
+    static int[][] matrizPoblacion = new int[numIndividuos][numGenes];
+    static double[] fitnessPoblacion = new double[numIndividuos];
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        crearPoblacionInicial();
+        imprimirPoblacion();
     }
+    
+    public static void crearPoblacionInicial() {
+        for (int i = 0; i < numIndividuos; i++) {
+            for (int j = 0; j < numGenes; j++) {
+                matrizPoblacion[i][j] = (Math.random() < 0.5) ? 1 : 0;
+            }
+        }
+    }
+
+    public static void imprimirPoblacion() {
+        for (int i = 0; i < numIndividuos; i++) {
+            System.out.print("[ ");
+            for (int j = 0; j < numGenes; j++) {
+                System.out.print(matrizPoblacion[i][j] + " ");
+            }
+            System.out.println("]");
+        }
+        System.out.println("");
+    }
+    
+    
 }
